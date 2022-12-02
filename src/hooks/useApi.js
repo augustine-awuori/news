2b170ed103c864776908ebb42cf7c7f99341478c
Augustine Awuori <codewithaugustine@gmail.com>
2022-12-02 17:18:07 +0300

    Fix the bug that didn't render articles.

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function useApi(apiFunc) {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
